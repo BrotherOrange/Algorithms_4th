1cfaf1737319c101a6ac5108c0ac4e3bcdfac91e
richard.zhang <zjh991600@163.com>
2022-07-06 19:09:02 -0700

    Graphs begins

---
+++ src/graphs/Tools.java
@@ -0,0 +1,29 @@
+package graphs;
+
+public abstract class Tools {
+
+    public static int degree(Graph g, int v) {
+        int degree = 0;
+        for (int w : g.adj(v)) degree++;
+        return degree;
+    }
+
+    public static int maxDegree(Graph g) {
+        int max = 0;
+        for (int v = 0; v < g.V(); v++)
+            if (degree(g, v) > max) max = degree(g, v);
+        return max;
+    }
+
+    public static double avgDegree(Graph g) {
+        return 2.0 * g.E() / g.V();
+    }
+
+    public static int numberOfSelfLoops(Graph g) {
+        int count = 0;
+        for (int v = 0; v < g.V(); v++)
+            for (int w : g.adj(v))
+                if (v == w) count++;
+        return count / 2;
+    }
+}
