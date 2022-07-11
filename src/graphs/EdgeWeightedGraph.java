679f2639479c7d2854b72953a77b7e5a00b0b728
richard.zhang <zjh991600@163.com>
2022-07-10 23:54:15 -0700

    Graphs 4.2 & 4.3 finished

---
+++ src/graphs/EdgeWeightedGraph.java
@@ -0,0 +1,73 @@
+package graphs;
+
+import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.In;
+
+import java.util.NoSuchElementException;
+
+public class EdgeWeightedGraph {
+
+    private final int V;
+    private int E;
+    private Bag<Edge>[] adj;
+
+    public EdgeWeightedGraph(int V) {
+        this.V = V;
+        this.E = 0;
+        adj = (Bag<Edge>[]) new Bag[V];
+        for (int v = 0; v < V; v++)
+            adj[v] = new Bag<>();
+    }
+
+    public EdgeWeightedGraph(In in) {
+        if (in == null) throw new IllegalArgumentException("argument is null");
+
+        try {
+            V = in.readInt();
+            adj = (Bag<Edge>[]) new Bag[V];
+            for (int v = 0; v < V; v++) {
+                adj[v] = new Bag<Edge>();
+            }
+
+            int E = in.readInt();
+            if (E < 0) throw new IllegalArgumentException("Number of edges must be non-negative");
+            for (int i = 0; i < E; i++) {
+                int v = in.readInt();
+                int w = in.readInt();
+                double weight = in.readDouble();
+                Edge e = new Edge(v, w, weight);
+                addEdge(e);
+            }
+        }
+        catch (NoSuchElementException e) {
+            throw new IllegalArgumentException("invalid input format in EdgeWeightedGraph constructor", e);
+        }
+
+    }
+    
+    public int V() {
+        return V;
+    }
+    
+    public int E() {
+        return E;
+    }
+
+    public Iterable<Edge> adj(int v) {
+        return adj[v];
+    }
+    
+    public void addEdge(Edge e) {
+        int v = e.either(), w = e.other(v);
+        adj[v].add(e);
+        adj[w].add(e);
+    }
+    
+    public Iterable<Edge> edges() {
+        Bag<Edge> b = new Bag<Edge>();
+        for (int v = 0; v < V; v++)
+            for (Edge e : adj[v])
+                if (e.other(v) > v) b.add(e);
+        return b;
+    }
+}
---
+++ src/graphs/Topological.java
@@ -0,0 +1,26 @@
+package graphs;
+
+/**
+ * 拓扑排序
+ */
+
+public class Topological {
+
+    private Iterable<Integer> order;
+
+    public Topological(Digraph g) {
+        DirectedCycle cycleFinder = new DirectedCycle(g);
+        if (!cycleFinder.hasCycle()) {
+            DepthFirstOrder dfs = new DepthFirstOrder(g);
+            order = dfs.reversePost();
+        }
+    }
+
+    public Iterable<Integer> order() {
+        return order;
+    }
+
+    public boolean isDAG() {
+        return order != null;
+    }
+}
